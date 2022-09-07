cde7b305680c40e99aca76b3377f8652ae4a0293
Sofi <sofijas@gmail.com>
2022-09-07 16:26:59 +0200

    sređene rute za rezervacije

--- skript_projekat_be/routes/rezervacija_routes.js
+++ skript_projekat_be/routes/rezervacija_routes.js
@@ -5,8 +5,7 @@ const bcrypt = require('bcrypt');
 require('dotenv').config();
 const { Op } = require("sequelize");
 
-const { sequelize, Termin, Myuser } = require('../models');
-const Rezervacija = require('../models/Rezervacija');
+const { sequelize, Termin, Myuser, Rezervacija } = require('../models');
 
 router.use(express.json());
 router.use(express.urlencoded({extended: true}));
@@ -29,22 +28,47 @@ function authToken(req, res, next) {
 
 router.use(authToken);
 
-// rezervisi trening
-router.post('/rt',(req, res) => {
+// napravi rezervaciju
+router.post('/',(req, res) => {
     let korId;
     Myuser.findOne({where: {username:req.body.username}})
-    .then (kor => korId=kor.id)
+    .then (kor => {
+        korId=kor.id
+        const rez ={
+            korisnikId: korId,
+            terminId: req.body.terminId
+        }
+        
+        Rezervacija.create(rez)
+        .then (data => {
+            Termin.findOne({where: {id: req.body.terminId}})
+            .then(trm => {
+                const novitermin={
+                    id: trm.id,
+                    dan: trm.dan,
+                    sati_od: trm.sati_od,
+                    sati_do: trm.sati_do,
+                    treningId: trm.treningId,
+                    masazaId: trm.masazaId,
+                    slobodna: trm.slobodna-1
+                }
+                Termin.update(novitermin, {where: {id: novitermin.id}})
+               .then(num =>{
+                   if (num==1){
+                       console.log("Uspesan update");
+                   } else {
+                       console.log("Nije dobar id");
+                   }
+               })
+               .catch(err =>{res.status(500).json(err)}); 
+
+            })
+        })
+    
+    })
     .catch (err => res.status(500).json(err));
 
-    const rez ={
-        korisnikId: korId,
-        terminId: req.body.terminId
-    }
-
-    Rezervacija.create(rez)
-    .then (data => {
-        //smanji slobodno
-    })
+    res.send("uspesano kreirana rezervacija");
 
 })
 
